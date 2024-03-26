0a428fd64b7c3fbd7149f642afa40fd3153a23f4
Pieter Claus <pieter.claus1@gmail.com>
2024-03-26 10:50:12 +0100

    use ft_strjoin to include path in the filename to open the right map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 10:05:51 by pclaus            #+#    #+#             */
-/*   Updated: 2024/03/25 11:27:56 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/03/26 10:48:22 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 int	main(int argc, char **argv)
 {
 	// int fd;
-	char *filename;
+	const char *filename;
+	const char *include_path;
 
 	if (argc == 2)
 	{
-		filename = argv[1];
+		include_path = "./maps/";
+		filename = ft_strjoin(include_path, (const char *)argv[1]);
 		ft_printf("The filename is: %s\n", filename);
+		free((void *)filename);
 	}
 }
