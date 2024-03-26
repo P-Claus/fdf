119d495681e7bcd4d5af5fb170624903c63ad7cb
Pieter Claus <pieter.claus1@gmail.com>
2024-03-26 18:17:58 +0100

    work on creating matric of correct size

---
+++ utils/calculate_matrix.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calculate_matrix.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 17:52:53 by pclaus            #+#    #+#             */
+/*   Updated: 2024/03/26 18:17:01 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+// returns a pointer to the matrix once it has been allocated on the heap?
+void	*calculate_matrix(int amount_of_rows, int amount_of_columns)
+{
+}
+
+int	calculate_amount_of_rows(void)
+{
+}
+
+int	calculate_amount_of_columns(void)
+{
+}
