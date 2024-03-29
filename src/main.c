02d004501a4fb5528892df4da67b03ea495a742d
Pieter Claus <pieter.claus1@gmail.com>
2024-03-29 17:08:32 +0100

    fixed leaks when looping through the file to create the matrix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 10:05:51 by pclaus            #+#    #+#             */
-/*   Updated: 2024/03/29 16:04:16 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/03/29 17:07:28 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,43 @@ int	main(int argc, char **argv)
 {
 	char	*filename;
 	char	*include_path;
-	int		fd;
+	int		fd_first_line;
 	char	*result;
-	char	*first_line;
-	int		amount_of_rows;
+	char	*temp;
 	int		amount_of_columns;
+	int		fd_result;
+	int		amount_of_rows;
 
-	// int fd;
 	amount_of_rows = 0;
 	amount_of_columns = 0;
 	if (argc == 2)
 	{
 		include_path = "./maps/";
 		filename = ft_strjoin(include_path, argv[1]);
-		fd = open(filename, O_RDONLY);
-		result = get_next_line(fd);
-		first_line = result;
+		fd_first_line = open(filename, O_RDONLY);
+		result = get_next_line(fd_first_line);
+		temp = result;
+		while (*result != '\0')
+		{
+			if (*result != 32)
+				amount_of_columns++;
+			result++;
+		}
+		free(temp);
+		close(fd_first_line);
+		fd_result = open(filename, O_RDONLY);
+		result = get_next_line(fd_result);
 		while (!result == 0)
 		{
-			result = get_next_line(fd);
-			amount_of_rows++;
 			free(result);
+			result = get_next_line(fd_result);
+			amount_of_rows++;
 		}
 		ft_printf("The amount of rows is: %d\n", amount_of_rows);
-		while (*first_line != '\0')
-		{
-			if (*first_line != 32)
-				amount_of_columns++;
-			first_line++;
-		}
 		ft_printf("The amount of columns is: %d\n", amount_of_columns - 1);
 		ft_printf("The result is: %s", result);
 		free(result);
-		close(fd);
+		close(fd_result);
 		free((void *)filename);
 	}
 }
